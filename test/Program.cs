ef18c74641ff35977e57098db2fb1b1a5208ef04
2022188 <2022188@B109-14>
2023-09-08 14:25:09 +0200

    Add project files.

---
+++ test/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //későbbi fejlesztés//
+            Console.WriteLine("Én vagyok a teszt program!");
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ test2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Én vagyok a teszt 2 program!");
+            Console.ReadKey();
+        }
+    }
+}
